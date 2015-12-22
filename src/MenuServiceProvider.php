b60901d25b45a03a5138525707c52a77a655af44
Aleksander Kalnoy <lazychaser@gmail.com>
2015-12-22 09:31:11 +0300

    Updated service provider

--- src/MenuServiceProvider.php
+++ src/MenuServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Illuminate\Html;
 
+use Illuminate\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
 
 class MenuServiceProvider extends ServiceProvider {
@@ -20,12 +21,15 @@ class MenuServiceProvider extends ServiceProvider {
 	 */
 	public function register()
 	{
-		$this->app->singleton('menu', function ($app)
+		$this->app->singleton('menu', function (Application $app)
         {
-            $menu = new MenuBuilder($app->make('request'));
+            $menu = new MenuBuilder;
 
             $menu->setUrlGenerator($app->make('url'));
             $menu->setTranslator($app->make('translator'));
+			$menu->setRequest($app->rebinding('request', function ($request) use ($menu) {
+				$menu->setRequest($request);
+			}));
 
             return $menu;
         });
